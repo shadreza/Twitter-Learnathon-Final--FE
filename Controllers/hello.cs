0a0a69f352b60c1dc0ae20ea1871647da0b5bb44
nasif <nasifshahriar4@gmail.com>
2022-05-17 21:02:05 +0600

    first

---
+++ Controllers/hello.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace first_task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class hello : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world");
+        }
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+    }
+}
