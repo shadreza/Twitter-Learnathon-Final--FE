9a425136c5b90addbd48b69e3b8a7fcb43657a09
shadreza <shadreza100@gmail.com>
2022-11-04 19:46:26 +0600

    The nested routing is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,20 @@ import {RouterModule, Routes} from '@angular/router';
 import { AuthLComponent } from './layouts/auth-l/auth-l.component';
 import { DashboardLComponent } from './layouts/dashboard-l/dashboard-l.component';
 import { ErrorPageLComponent } from './layouts/error-page-l/error-page-l.component';
+import { LoginComponent } from './modules/user-credential-m/login/login.component';
+import { SignupComponent } from './modules/user-credential-m/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: DashboardLComponent },
-  { path: 'auth', component: AuthLComponent },
+  {
+    path: 'auth',
+    component: AuthLComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'login'},
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+    ]
+  },
   { path: '**', component: ErrorPageLComponent },
 ];
 
