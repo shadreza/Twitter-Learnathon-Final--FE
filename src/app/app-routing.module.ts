c7d45a27b183a16e1ea26a999decf65bfe50e2f6
shadreza <shadreza100@gmail.com>
2022-11-18 21:23:50 +0600

    Starting from the initial point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-    {path: '', pathMatch: 'full', redirectTo: 'login'}, // Route Guard
+    // {path: '', pathMatch: 'full', redirectTo: 'login'}, // Route Guard
     // {path: '**', component: NotFoundComponent}, //Wild Card Route for 404 request, must be in bottom
     // loadChildren:() => import('./login/login.module').then(_ => _.LoginModule)  // for lazy-loading of that module
 ];
